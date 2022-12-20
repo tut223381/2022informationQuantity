5382f3aa86f393be6c185819a21df2112540de92
TUT3Software4Project <tut.umemura.lab@gmail.com>
2022-12-20 16:42:04 +0900

    javadoc fix

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"s4.specification"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"s4.specification","l":"FrequencerInterface"},{"p":"s4.specification","l":"InformationEstimatorInterface"}];updateSearchResults();
